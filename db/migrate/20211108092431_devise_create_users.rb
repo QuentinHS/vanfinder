b02e663bcd541f1e318827963c7e8583048a35b6
QuentinHS <quentin.halysummerfield@gmail.com>
2021-11-08 19:31:52 +1000

    fixing configuration issues

--- db/migrate/20211108092431_devise_create_users.rb
+++ db/migrate/20211108092431_devise_create_users.rb
@@ -34,8 +34,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
 
       t.string :first_name
       t.string :last_name
-      t.boolean :admin, default: false
-
       t.timestamps null: false
     end
 
