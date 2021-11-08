91404698ac91f6e379ab83acd1374c31338117e0
QuentinHS <quentin.halysummerfield@gmail.com>
2021-11-08 22:23:56 +1000

    added cloudinary config settings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,11 @@
 class Listing < ApplicationRecord
   belongs_to :user
+
+  has_one_attached :listing_image
+
+  validates :price, presence: { message: "Please provide a price"}
+  validates :description, presence: { message: "Please add a description"}
+  validates :postcode, presence: { message: "Please give a postcode"}
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :listings, dependent: :destroy
+
   validates_uniqueness_of :username
   validates_presence_of :username
 end
