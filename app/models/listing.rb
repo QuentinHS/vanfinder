311eb8992acb2ca8b2b6155a93ba009661f61cff
QuentinHS <quentin.halysummerfield@gmail.com>
2021-11-13 13:11:37 +1000

    modified listing form

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,11 +7,13 @@ class Listing < ApplicationRecord
   # after_create :add_editor_role
 
 
-  # validates :listing_image, attached: true, content_type: %i[png jpg jpeg]
+  validates :listing_image, attached: true, content_type: %i[png jpg jpeg], presence: { message: "Please provide an image of your van"}
   validates :price, presence: { message: "Please provide a price"}
   validates :description, presence: { message: "Please add a description"}
-  validates :postcode, presence: { message: "Please give a postcode"}
   validates :description, length: {minimum:10, maximum:500}
+  validates :city, presence: { message: "Please provide your city or town"}
+  validates :state, presence: { message: "Please provide your state"}
+ 
 
   private
 
