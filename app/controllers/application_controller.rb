578146ca2e52bf3b2d13c0b60689ddc25702b310
QuentinHS <quentin.halysummerfield@gmail.com>
2021-11-13 23:13:50 +1000

    added profile link from show listing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  # add phone number to user class/profile
+  # Get rid of new listing in user profile
+  # Create van class with parameters from add, including new parameter seating
+  # Create van ammenities classes
+  # Link profile to user listings
+  # Add search functionality
+  # 
+
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
