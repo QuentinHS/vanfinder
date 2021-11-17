64e6d17b5682851191e276aace77f08e228ede23
QuentinHS <quentin.halysummerfield@gmail.com>
2021-11-17 11:21:50 +1000

    minor fixes

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -84,9 +84,8 @@ class ListingsController < ApplicationController
     # def listing_params
 
 
-
     def listing_params
-      params.require(:listing).permit(:city, :state, :sold, :description, :user_id, :price, :listing_image, van_attributes: [:make, :model, :year, :odometer, :fuel_type, :listing_id, :type, :roof_type, :sleeps, :seats, amenities_vans_attributes: [:van_id, :amenity_id, :created_at, :updated_at, :_destroy]])
+      params.require(:listing).permit(:city, :state, :sold, :description, :user_id, :price, :listing_image, van_attributes: [:make, :model, :year, :odometer, :fuel_type, :listing_id, :type, :roof_type, :sleeps, :seats, amenities_vans_attributes: [ amenity_attributes: [:name] ]])
       
     end
 
