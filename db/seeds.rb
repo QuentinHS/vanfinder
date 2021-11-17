d795bc66ff692b7be9ae11c9c92c19c7aaa8b357
QuentinHS <quentin.halysummerfield@gmail.com>
2021-11-18 01:44:45 +1000

    added database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Listing.destroy_all
+Amenity.destroy_all
+
+amenities = [
+    Amenity.create!(name: "shower"),
+    Amenity.create!(name: "air conditioning"),
+    Amenity.create!(name: "heater"),
+    Amenity.create!(name: "fan"),
+    Amenity.create!(name: "solar"),
+    Amenity.create!(name: "inverter"),
+    Amenity.create!(name: "stove"),
+    Amenity.create!(name: "oven"),
+    Amenity.create!(name: "fridge"),
+    Amenity.create!(name: "bike rack"),
+    Amenity.create!(name: "water tank"),
+    Amenity.create!(name: "sink"),
+    Amenity.create!(name: "tv")
+]
