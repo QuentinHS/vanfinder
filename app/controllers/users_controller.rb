8f7101096fc4cf1127d0d88451f61b4c83497cf3
QuentinHS <quentin.halysummerfield@gmail.com>
2021-11-25 04:06:38 +1000

    Got messaging working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: %i[ show ]
-  
+
   def show
     @user = User.find(params[:id])
   end
-
 
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'listings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show], as: "profile" 
+  resources :users, as: "profile" 
 
   resources :users do
-    # :profile
+    :profile
     resources :messages, only: [:new, :create] #-> domain.com/users/:user_id/messages/new
   end
    
