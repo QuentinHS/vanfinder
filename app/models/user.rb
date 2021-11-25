2f96b0cab913abaf8b4bfe1e80fa63acd0bf4328
QuentinHS <quentin.halysummerfield@gmail.com>
2021-11-25 16:40:09 +1000

    added slugs to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :messages, class_name: "Message", foreign_key: "recipient_id"
   has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
 
+  extend FriendlyId
+  friendly_id :username, use: :slugged
+
   rolify
 
   # Create new model called profile with one to one relationship to user, can put custom stuff in the profile without worrying about devise
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_061708) do
+ActiveRecord::Schema.define(version: 2021_11_25_063642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,8 +112,10 @@ ActiveRecord::Schema.define(version: 2021_11_24_061708) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
     t.integer "phone_number"
+    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
